8436607b59a12212c33a38d2c0788a44252475e1
Locon213 <167447571+Locon213@users.noreply.github.com>
2024-10-27 20:29:18 +0300

    Update plugin.py

--- libs/ColorChange(Example)/plugin.py
+++ libs/ColorChange(Example)/plugin.py
@@ -1,4 +1,12 @@
-print("Hello, World!")
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QColor
 
 def run():
-    print("Плагин запущен!")
+    app = QApplication.instance()
+    if app:
+        # Меняем цвет фона главного окна
+        new_color = QColor(100, 150, 200)  # Задаем новый цвет (RGB)
+        app.setStyleSheet(f"background-color: {new_color.name()};")
+        print('Цвет фона изменен!')
+    else:
+        print('Приложение не запущено, цвет фона не может быть изменен.')
